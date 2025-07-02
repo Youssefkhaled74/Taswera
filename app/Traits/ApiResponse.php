336948f14363610f3f5d56a0d46483c0985477ed
YoussefKhaled74 <youremail@example.com>
2025-07-02 13:17:16 +0300

    Add intervention/image dependency to composer.json and update composer.lock

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponse
+{
+    public function successResponse(int $status = 200,  string $message = 'Success', $data = null): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
+    public function errorResponse(int $status = 500 , string $message, $data = null): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
+}
+    
